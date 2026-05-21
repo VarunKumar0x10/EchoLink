fa189524272ce647623ef00e7743ffae8db5a019
VarunKumar0x10 <varundinoking27@gmail.com>
2026-05-22 00:52:31 +0530

    Minor fixes

--- linux/main.cpp
+++ linux/main.cpp
@@ -18,9 +18,9 @@
 #include <string.h>
 
 // Dear ImGui
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_glfw.h"
-#include "imgui/backends/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 // ============================================================
 // HELPERS & DATA STRUCTURES
